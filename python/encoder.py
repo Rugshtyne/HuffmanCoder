740ddc6d60fda357dd2aff217a1e2fed9578d469
AurimasVilys <aurimas.vilys.96@gmail.com>
2019-01-09 23:47:58 +0200

    Work on encoder

--- python/encoder.py
+++ python/encoder.py
@@ -6,7 +6,19 @@ isRemaining = 0
 K = int(sys.argv[2])
 currentWord = ""
 byteStringLeftover = ""
+frequencyTable = []
+lookupTable = []
+treeInLine = ''
 
+class Node():
+	def __init__(self, character, freq, left, right):
+		self.character = character
+		self.freq = freq
+		self.left = left
+		self.right = right
+
+	def checkIfLeaf(self):
+		return (self.left == None and self.right == None)
 
 
 # my_bytes = [int(x, 2) for x in my_bytes]
@@ -14,12 +26,6 @@ byteStringLeftover = ""
 # newFile = open("test.bin", "w+b")
 # byteArray = bytearray(my_bytes)
 # newFile.write(byteArray)
-
-def createFreqTable(byteString):
-	try:
-		
-	except Exception as e:
-		raise e
 
 def processByteStringLeftover(file=None):
 	global byteStringLeftover
@@ -42,6 +48,7 @@ def processByteStringLeftover(file=None):
     #             }   
 			else:
 				print(currentWord)
+				createFreqTable(currentWord)
 				# !!! createFreqTable(currentWord)
 			currentWord = ""
 		else:
@@ -62,23 +69,67 @@ def processByte(byte, file=None):
 		byteStringLeftover = binary_str[compareTo:8]
 		if file is not None:
 			pass
-			# NEPERRASYTA IS JAVOS
-			# !!! for(LookupTable record : this.lookupTable) {
-	#              if(record.getCharacter().equals(this.currentWord)) {
-	#                  this.appendFile(file, record.getTreePath());
-	#                  break;
-	#              }
-	#          }
+			record = [rec for rec in lookupTable if (rec['Character'] == currentWord)]
 		else:
-   			print(currentWord)
-   			# NEPERRASYTA IS JAVOS
-   			# !!! createFreqTable(currentWord)
+   			createFreqTable(currentWord)
 		currentWord = ""
 		if isRemaining != 0:
 			isRemaining = 0
 	else:
 		currentWord += binary_str
 		isRemaining = compareTo - 8
+
+
+def createFreqTable(byteString):
+	record = [rec for rec in frequencyTable if (rec['Sequence'] == byteString)]
+	if (len(record) == 0):
+		record = { "Sequence": byteString, "Frequence": 1}
+		frequencyTable.append(record)
+	else:
+		record[0]["Frequence"] = record[0]["Frequence"] + 1
+
+def CreateTree():
+	treeArray = []
+	for record in frequencyTable:
+		node = Node(record['Sequence'], record['Frequence'], None, None)
+		treeArray.append(node)
+
+	while len(treeArray) > 1:
+		left = removeMinFreq(treeArray)
+		right = removeMinFreq(treeArray)
+		parent = Node(None,left.freq+right.freq,left,right)
+		treeArray.append(parent)
+	return treeArray[0]
+
+def removeMinFreq(list):
+	list.sort(key=lambda x: x.freq)
+	returnNode = list[0]
+	list.remove(returnNode)
+	return returnNode
+
+def buildCode(node, line):
+	if(node.checkIfLeaf() == False):
+		buildCode(node.left, line + "0")
+		buildCode(node.right, line + "1")
+	else:
+		record = [rec for rec in lookupTable if (rec['Character'] == node.character)]
+		if (len(record) == 0):
+			record = { "Character": node.character, "Path": line}
+			lookupTable.append(record)
+		else:
+			record[0]["Path"] = line
+
+def printTree(tree):
+	global treeInLine
+	if(tree != None):
+		if (tree.left == None and tree.right == None):
+			treeInLine = treeInLine + '0'
+			treeInLine = treeInLine + tree.character
+		else:
+			treeInLine = treeInLine + '1'
+			printTree(tree.left)
+			printTree(tree.right)
+
 
 with open(sys.argv[1], "rb") as f:
 	b = f.read(1)
@@ -89,7 +140,16 @@ with open(sys.argv[1], "rb") as f:
 	processByteStringLeftover()
 	if currentWord:
 		pass
-		# NEPERRASYTA IS JAVOS
-		# !!! createFreqTable(currentWord); 
+		createFreqTable(currentWord)
 	isRemaining = 0
 	currentWord = ""
+	#print (frequencyTable)
+	root = CreateTree()
+	print(root.right)
+	buildCode(root,'')
+	printTree(root)
+	print(treeInLine)
+
+with open(sys.argv[1]+'.huf', 'wb') as w:
+	#w.write()
+	w.close()
